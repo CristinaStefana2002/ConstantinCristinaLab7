9f375260d46eaa118070bc9b65f14da427f11d4d
Cristina Constantin <constantin.cristina2002@gmail.com>
2024-11-25 15:07:16 +0200

    Commit 1

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,27 @@
-﻿namespace ConstantinCristinaLab7
+﻿using System;
+using ConstantinCristinaLab7.Data;
+using System.IO;
+
+namespace ConstantinCristinaLab7
 {
     public partial class App : Application
+    
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
